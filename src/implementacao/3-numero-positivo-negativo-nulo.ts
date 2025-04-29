1977929f4021121b5c426de0123b2081ef73895f
Jhamyllie <milli.santana2017@gmail.com>
2025-04-29 11:40:40 -0300

    Adiciona Exercício numero positivo, negativo, nulo

--- src/implementacao/3-numero-positivo-negativo-nulo.ts
+++ src/implementacao/3-numero-positivo-negativo-nulo.ts
@@ -1,4 +1,14 @@
-function verificarNumero() {
-    return null;
+function verificarNumero(numero: number): string {
+    if (numero > 0) {
+        return "POSITIVO";
+    } else if(numero < 0) {
+        return "NEGATIVO"
+    }else{
+        return "NULO";
+    }
+
 }
 
+// console.log(verificarNumero(5));
+// console.log(verificarNumero(-3));
+// console.log(verificarNumero(0));
