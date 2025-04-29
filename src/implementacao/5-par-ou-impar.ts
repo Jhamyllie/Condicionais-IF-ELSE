3b6709577ff8cf3550429b76cfda5f0338819f54
Jhamyllie <milli.santana2017@gmail.com>
2025-04-29 13:19:57 -0300

    Adiciona Exercício par ou ímpar

--- src/implementacao/5-par-ou-impar.ts
+++ src/implementacao/5-par-ou-impar.ts
@@ -1,4 +1,19 @@
-function verificarParOuImpar() {
-    return null;
+function verificarParOuImpar(numero: number): string{
+    if(numero % 2 == 0){
+        return "PAR";
+    } else{
+        return "IMPAR";
+    }
 }
 
+console.log(verificarParOuImpar(4));
+console.log(verificarParOuImpar(7));
+console.log(verificarParOuImpar(0));
+console.log(verificarParOuImpar(-15));
+console.log(verificarParOuImpar(-10));
+
+/*
+Se o número fornecido for 4, o algoritmo deve imprimir "PAR".
+Se o número fornecido for 7, o algoritmo deve imprimir "ÍMPAR".
+Se o número fornecido for 0, o algoritmo deve imprimir "PAR", pois 0 é divisível por 2.
+*/
