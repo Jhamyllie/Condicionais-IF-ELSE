1424386f8ad48f69a10dd158df414739cfd71990
Jhamyllie <milli.santana2017@gmail.com>
2025-04-29 10:50:07 -0300

    Adiciona Exercício soma maior que 50

--- src/implementacao/1-soma-maior-que-50.ts
+++ src/implementacao/1-soma-maior-que-50.ts
@@ -1,3 +1,12 @@
-function somaMaiorQueCinquenta() {
-    return null;
+function somaMaiorQueCinquenta(num1: number, num2: number): string {
+    let soma: number = num1 +num2; 
+    if (soma > 50) {
+        return String(soma)
+    } else {
+        return "null"
+    }
 }
+
+// console.log(somaMaiorQueCinquenta(5,10));
+// console.log(somaMaiorQueCinquenta(25,30));
+// console.log(somaMaiorQueCinquenta(20,25));
