a4314a8e4f5fb3b9260ec35a903b67315e6d431e
TiciB <tebezerra@gmail.com>
2025-04-29 13:27:22 -0300

    Adicionando Exercício Múltiplo de 3

--- src/implementacao/6-multiplo-de-3.ts
+++ src/implementacao/6-multiplo-de-3.ts
@@ -1,4 +1,15 @@
-function verificarMultiploDe3() {
-    return null;
+function verificarMultiploDe3(num: number): string {
+    if( num % 3 === 0){
+        return "É multiplo de 3";
+    }else{
+        return "Não é multiplo de 3";
+    }
 }
 
+console.log(verificarMultiploDe3(9));
+console.log(verificarMultiploDe3(10));
+console.log(verificarMultiploDe3(0))
+
+/*Se o número fornecido for 9, o algoritmo deve imprimir "é múltiplo de 3".
+Se o número fornecido for 10, o algoritmo deve imprimir "não é múltiplo de 3".
+Se o número fornecido for 0, o algoritmo deve imprimir "é múltiplo de 3", pois 0 é divisível por 3.*/
