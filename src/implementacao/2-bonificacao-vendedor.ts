4aa277abaaed39f0f3d1d785e0adeb82aa6b0d9c
TiciB <tebezerra@gmail.com>
2025-04-29 11:17:23 -0300

    Adicionando Exercício Bonificação Vendedor

--- src/implementacao/2-bonificacao-vendedor.ts
+++ src/implementacao/2-bonificacao-vendedor.ts
@@ -1,4 +1,14 @@
-function calcularBonificacao() {
-    return null;
+function calcularBonificacao(valor: number, quantidade: number): string {
+    let total: number = valor*quantidade;
+
+    if (total > 100) {
+        return String("Valor total: R$ " + total.toFixed(2) + " Bonificação de 10% para o vendedor!");
+    }else{
+        return String("Valor total: R$ " + total.toFixed(2));
+    }
+
 }
+
+    console.log(calcularBonificacao(20,6))
+    console.log(calcularBonificacao(15,4))
 
