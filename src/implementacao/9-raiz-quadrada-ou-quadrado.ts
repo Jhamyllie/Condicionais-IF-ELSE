f414bc29cddc66dac043951807ff8b75dfb1a0a9
Jhamyllie <milli.santana2017@gmail.com>
2025-05-02 11:32:27 -0300

    Adiciona exrecício raiz quadrada ou quadrado

--- src/implementacao/9-raiz-quadrada-ou-quadrado.ts
+++ src/implementacao/9-raiz-quadrada-ou-quadrado.ts
@@ -1,4 +1,25 @@
-function calcularRaizOuQuadrado() {
-    return null;
+function calcularRaizOuQuadrado(numero: number): number {
+    let resultado: number;
+    let i: number = 0;
+
+    if (numero >= 0) {
+        while (i * i <= numero) {
+            i++;
+        }
+        resultado = i - 1;
+    } else {
+        resultado = numero * numero;
+    }
+    return resultado;
 }
 
+console.log(calcularRaizOuQuadrado(16));
+console.log(calcularRaizOuQuadrado(-3));
+console.log(calcularRaizOuQuadrado(0));
+console.log(calcularRaizOuQuadrado(11));
+
+/*
+Se o número fornecido for 16, o algoritmo deve imprimir a raiz quadrada de 16, que é 4.
+Se o número fornecido for -3, o algoritmo deve imprimir o quadrado de -3, que é 9.
+Se o número fornecido for 0, o algoritmo deve imprimir a raiz quadrada de 0, que é 0.
+*/
